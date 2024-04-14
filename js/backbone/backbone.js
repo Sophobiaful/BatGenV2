2cb09152ed2685ff40b547ec67c58f62a0ea9c36
Kyle C. <kcross199@gmail.com>
2024-04-14 00:43:26 -0400

    Update backbone.js

--- js/backbone/backbone.js
+++ js/backbone/backbone.js
@@ -357,7 +357,7 @@ var backboneView_Fields = Backbone.View.extend({
 			if (curModel.get('type') === 'video') {
 				html += temp_Video({
 					uid: curModel.get('uid'),
-					counter: (i+1),
+					counter: 2,
 					textbox: {
 						item:temp_Textbox,
 						variables:{
